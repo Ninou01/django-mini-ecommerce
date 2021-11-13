4a97270a48a13ec9c634dda453d8d2df2d80f48a
Ninou01 <ninofcm20@gmail.com>
2021-11-13 20:51:53 +0100

    preparing site for deployment on heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+DEVELOPMENT_MODE = str(os.environ.get('DEVELOPMENT_MODE')) == "1"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
                           "127.0.0.1,localhost").split(",")
