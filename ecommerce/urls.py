fe3fc483a222dbc347ceac0330ceded1d45a0069
Ninou01 <ninofcm20@gmail.com>
2022-02-01 00:14:27 +0100

    redirect from the main url to shop page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,11 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
+
+def redirect_shop_page(request):
+    return redirect('/shop')
 
 urlpatterns = [
     path('admin', admin.site.urls),
     path('shop', include('shop.urls')),
-
+    path('', redirect_shop_page),
 ]
 
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
