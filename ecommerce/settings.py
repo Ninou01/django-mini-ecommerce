29f55cf04f707f7553b92a51e4bea7a2eb3f025e
Ninou01 <ninofcm20@gmail.com>
2022-02-24 18:32:17 +0100

    installing django_cleanup to delet images from server when the model is deleted or it's image is deleted

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'shop.apps.ShopConfig',
     'storages',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
--- shop/models.py
+++ shop/models.py
@@ -68,33 +68,3 @@ class Commande(models.Model):
         super(Commande, self).save(*args, **kwargs)
 
 
-@receiver(models.signals.post_delete, sender=Produit)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    """
-    Deletes file from filesystem
-    when corresponding `Produit` object is deleted.
-    """
-    if instance.image:
-        if os.path.isfile(instance.image.name):
-            os.remove(instance.image.name)
-
-
-@receiver(models.signals.pre_save, sender=Produit)
-def auto_delete_file_on_change(sender, instance, **kwargs):
-    """
-    Deletes old file from filesystem
-    when corresponding `Produit` object is updated
-    with new file.
-    """
-    if not instance.pk:
-        return False
-
-    try:
-        old_file = Produit.objects.get(pk=instance.pk).image
-    except Produit.DoesNotExist:
-        return False
-
-    new_file = instance.image
-    if not old_file == new_file:
-        if os.path.isfile(old_file.name):
-            os.remove(old_file.name)
